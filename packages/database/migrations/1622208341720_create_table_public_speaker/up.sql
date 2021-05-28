b0ff22141494adce3359c1421aeb4f18c64f3d4c
Tymoteusz Czech <mail@tymek.cz>
2021-05-28 18:32:59 +0200

    add speakers migration

---
+++ packages/database/migrations/1622208341720_create_table_public_speaker/up.sql
@@ -0,0 +1,18 @@
+CREATE TABLE "public"."speaker" ("id" uuid NOT NULL DEFAULT gen_random_uuid(), "created_at" timestamptz NOT NULL DEFAULT now(), "updated_at" timestamptz NOT NULL DEFAULT now(), "name" text NOT NULL, "description" text, "image" text, PRIMARY KEY ("id") );
+CREATE OR REPLACE FUNCTION "public"."set_current_timestamp_updated_at"()
+RETURNS TRIGGER AS $$
+DECLARE
+  _new record;
+BEGIN
+  _new := NEW;
+  _new."updated_at" = NOW();
+  RETURN _new;
+END;
+$$ LANGUAGE plpgsql;
+CREATE TRIGGER "set_public_speaker_updated_at"
+BEFORE UPDATE ON "public"."speaker"
+FOR EACH ROW
+EXECUTE PROCEDURE "public"."set_current_timestamp_updated_at"();
+COMMENT ON TRIGGER "set_public_speaker_updated_at" ON "public"."speaker" 
+IS 'trigger to set value of column "updated_at" to current timestamp on row update';
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
