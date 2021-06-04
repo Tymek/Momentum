4c1ead7f7685773c0549d00b048b01f17711a1ae
Tymoteusz Czech <mail@tymek.cz>
2021-06-04 22:34:36 +0200

    fix eslint tests

--- packages/eslint/tests/styled-no-partial-interpolation.test.ts
+++ packages/eslint/tests/styled-no-partial-interpolation.test.ts
@@ -1,30 +1,33 @@
-import rule from '../lib/rules/styled-no-partial-interpolation'
+import { resolve } from 'path'
 import { RuleTester } from 'eslint'
+import rule from '../lib/rules/styled-no-partial-interpolation'
 
-const ruleTester = new RuleTester()
+const ruleTester = new RuleTester({
+  parser: resolve(__dirname, '..', 'node_modules', '@typescript-eslint', 'parser'),
+})
+
 ruleTester.run('styled-no-partial-interpolation', rule, {
   valid: [
     {
       code:
-        'const Wrapper = styled.View`' +
-        'background: ${({ theme }) => theme.color.gray};' +
-        'padding: ${({ theme }) => `${theme.spacing.m}px`};' +
-        'margin-bottom: ${({ theme }) => `${theme.spacing.m}px`};' +
+        'const Wrapper = styled.View`\n' +
+        '  background: ${({ theme }) => theme.color.gray};\n' +
+        '  padding: ${({ theme }) => `${theme.spacing.m}px`};\n' +
+        '  margin-bottom: ${({ theme }) => `${theme.spacing.m}px`};\n' +
         '`;',
     },
   ],
-
   invalid: [
     {
       code:
-        'const Wrapper = styled.View`' +
-        'background: ${({ theme }) => theme.color.gray};' +
-        'padding: ${({ theme }) => theme.spacing.m}px;' +
-        'margin-bottom: ${({ theme }) => `${theme.spacing.m}px`};' +
+        'const Wrapper = styled.View`\n' +
+        '  background: ${({ theme }) => theme.color.gray};\n' +
+        '  padding: ${({ theme }) => theme.spacing.m}px;\n' +
+        '  margin-bottom: ${({ theme }) => `${theme.spacing.m}px`};\n' +
         '`;',
       errors: [
         {
-          message: 'Should fail on interpolating padding',
+          message: 'Do not use CSS units in this way. See `packages/eslint/docs`',
         },
       ],
     },
