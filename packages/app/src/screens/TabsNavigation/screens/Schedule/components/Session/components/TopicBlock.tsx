975363c967ebd095a0f4a811f22fbc1a3511cf19
Bartosz Kazuła <bartosz.kazula@gmail.com>
2021-06-29 19:31:42 +0200

    Code cleanup after moving the expand option

--- packages/app/src/screens/TabsNavigation/screens/Schedule/components/Session/components/TopicBlock.tsx
+++ packages/app/src/screens/TabsNavigation/screens/Schedule/components/Session/components/TopicBlock.tsx
@@ -1,16 +1,9 @@
-import React, { FC, useEffect, useState } from 'react'
-import { View } from 'react-native'
-import Animated, {
-  useAnimatedStyle,
-} from 'react-native-reanimated'
-import { useTheme } from '@react-navigation/native'
-import { MaterialIcons } from '@expo/vector-icons'
+import React, { FC } from 'react'
 
 import { Maybe, Scalars, SpeakerFragment } from '@-local/db/lib/api'
 import Text from 'components/Text'
 import useShadow from 'hooks/useShadow'
-import { TouchableInfo, Wrapper } from './Block'
-import { useSpringTransition } from 'hooks/useSpringTransition'
+import { Info, Wrapper } from './Block'
 import ExtendedInfo from './ExtendedInfo'
 
 type SessionProps = {
@@ -26,41 +19,19 @@ type SessionProps = {
 const TopicBlock: FC<SessionProps> = ({
   title,
   description,
-  /*ends_at,*/
   children,
   speaker,
   location,
   muted,
 }) => {
-  const theme = useTheme()
   const shadow = useShadow(2)
-  const [isOpen, setIsOpen] = useState<boolean>()
-  const [height, setHeight] = useState<number>()
-  const transition = useSpringTransition(isOpen, height)
-  const style = useAnimatedStyle(() => ({
-    height: transition.value,
-  }))
 
   return (
     <Wrapper style={!muted ? shadow : {}} muted={muted}>
-      <TouchableInfo onPress={() => setIsOpen(!isOpen)}>
+      <Info>
         <Text>{title}</Text>
-        <MaterialIcons
-          name={isOpen ? 'unfold-less' : 'unfold-more'}
-          size={24}
-          color={theme.colors.text}
-        />
-      </TouchableInfo>
-
-      <Animated.View style={[{ overflow: 'hidden' }, style]}>
-        <View
-          onLayout={(event) => {
-            setHeight(event?.nativeEvent?.layout?.height)
-          }}
-        >
-          <ExtendedInfo {...{ description, location, speaker, children }} />
-        </View>
-      </Animated.View>
+      </Info>
+      <ExtendedInfo {...{ description, location, speaker, children }} />
     </Wrapper>
   )
 }
