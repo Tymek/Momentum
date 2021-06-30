db82fe43440800b89d96a468b7480de61dce5db6
Tymoteusz Czech <mail@tymek.cz>
2021-06-30 21:18:50 +0200

    error component reload

--- packages/app/src/containers/error/ErrorComponent.tsx
+++ packages/app/src/containers/error/ErrorComponent.tsx
@@ -1,29 +1,49 @@
 import React, { FC } from 'react'
-import Markdown from 'components/Markdown'
+import { Button, Platform } from 'react-native'
+import { useTheme } from '@emotion/react'
+import styled from '@emotion/native'
 
+import Markdown from 'components/Markdown'
 import TextPage from 'components/TextPage'
 
-const messages = {
-  unknownError: 'Wystąpił nieznany błąd',
-}
+// const messages = {
+//   unknownError: 'Wystąpił nieznany błąd',
+// }
 
 type ErrorComponentProps = {
   message?: string
 }
 
 const ErrorComponent: FC<ErrorComponentProps> = ({ message }) => {
+  const theme = useTheme()
   const text = `
 # Wystąpił błąd 🥺
 Coś jest nie tak po naszej stronie.
-
-## _${message && typeof message === 'string' ? message : messages.unknownError}_
 `
+  console.error('error', message)
+  // ## _${message && typeof message === 'string' ? message : messages.unknownError}_
 
   return (
     <TextPage>
       <Markdown>{text}</Markdown>
+      {Platform.OS === 'web' && (
+        <ButtonView>
+          <Button
+            color={theme.color.primary}
+            onPress={() => {
+              window?.location.reload()
+              return false
+            }}
+            title="Załaduj ponownie"
+          />
+        </ButtonView>
+      )}
     </TextPage>
   )
 }
+
+const ButtonView = styled.View`
+  margin: ${({ theme }) => `${theme.spacing.m}px auto`};
+`
 
 export default ErrorComponent
