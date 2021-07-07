d9b458465b85ed64840c7dbe16b533090a531111
Tymoteusz Czech <mail@tymek.cz>
2021-07-07 18:10:30 +0200

    geo links

--- packages/app/src/screens/TabsNavigation/screens/Map/utils/systemNavigate.ts
+++ packages/app/src/screens/TabsNavigation/screens/Map/utils/systemNavigate.ts
@@ -9,6 +9,24 @@ const navigate = (lat: number, lng: number, title?: string): void => {
     web: `https://www.google.com/maps/dir//${title ? encodeURI(title) : latLng}/@${latLng},13z`,
   })
 
+  if (Platform.OS === 'web') {
+    if (/(android)/i.test(navigator.userAgent)) {
+      Linking.openURL(`geo:0,0?q=${title ? title.replace(' ', '+') : latLng}(${title})`)
+      return
+    }
+    if (
+      ['iPad Simulator', 'iPhone Simulator', 'iPod Simulator', 'iPad', 'iPhone', 'iPod'].includes(
+        navigator.platform,
+      ) ||
+      (navigator.userAgent.includes('Mac') && 'ontouchend' in document)
+    ) {
+      Linking.openURL(
+        `http://maps.apple.com/?daddr=${title ? title.replace(' ', '+') : latLng}&ll=${latLng}`,
+      )
+      return
+    }
+  }
+
   if (url) {
     Linking.openURL(url)
   }
