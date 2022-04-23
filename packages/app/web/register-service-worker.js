90f91aeb60766930a98d5e65970a1219ad3ab30c
Tymoteusz Czech <2625371+Tymek@users.noreply.github.com>
2022-04-23 16:20:30 +0200

    test unloading service worker for admin panel

--- packages/app/web/register-service-worker.js
+++ packages/app/web/register-service-worker.js
@@ -2,10 +2,29 @@
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function () {
+    if (window.location.pathname.startsWith('/panel')) {
+      let reload = false
+      navigator.serviceWorker.getRegistrations().then((registrations) => {
+        for (let registration of registrations) {
+          registration.unregister()
+          reload = true
+        }
+      })
+      if (reload) {
+        window.location.reload()
+      }
+      return
+    }
+
     navigator.serviceWorker
       .register('SW_PUBLIC_URL/expo-service-worker.js', { scope: 'SW_PUBLIC_SCOPE' })
       .then(function (info) {
         console.info('Registered service-worker', info)
+
+        if (window.location.pathname.startsWith('/panel')) {
+          info.unregister()
+          window.location.reload()
+        }
       })
       .catch(function (error) {
         console.info('Failed to register service-worker', error)
